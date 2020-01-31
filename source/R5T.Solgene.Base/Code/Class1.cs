84d813972abd21b0803eade1570b3167280f3ede
MinexAutomation <minex.automation@gmail.com>
2020-01-31 12:41:27 -0800

    Added IVisualStudioSolutionFileGenerator service definition interface type.

--- source/R5T.Solgene.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Solgene.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Solgene.Base/Code/Services/Definitions/IVisualStudioSolutionFileGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.Cambridge.Types;
+
+
+namespace R5T.Solgene
+{
+    /// <summary>
+    /// A service that generates in-memory <see cref="SolutionFile"/> instances.
+    /// </summary>
+    public interface IVisualStudioSolutionFileGenerator
+    {
+        SolutionFile GenerateSolutionFile();
+    }
+}
