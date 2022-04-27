9dbd49ae41f5d821bc29c29702504743a0472ea0
davidcoats <david@getsafetycone.com>
2022-04-27 15:35:06 -0700

    Interim changes.

--- source/R5T.Solgene.Base/Code/Services/Definitions/IVisualStudioSolutionFileGenerator.cs
+++ source/R5T.Solgene.Base/Code/Services/Definitions/IVisualStudioSolutionFileGenerator.cs
@@ -1,14 +1,14 @@
-﻿using System;
+using System;
 
-using R5T.Cambridge.Types;
+using R5T.Cambridge.Types;using R5T.T0064;
 
 
 namespace R5T.Solgene
-{
+{[ServiceDefinitionMarker]
     /// <summary>
     /// A service that generates in-memory <see cref="SolutionFile"/> instances.
     /// </summary>
-    public interface IVisualStudioSolutionFileGenerator
+    public interface IVisualStudioSolutionFileGenerator:IServiceDefinition
     {
         SolutionFile GenerateVisualStudioSolutionFile();
     }
